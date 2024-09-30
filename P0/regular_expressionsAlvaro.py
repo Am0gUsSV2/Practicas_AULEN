d39ea5676c9d6bc8b9c3c538cbc3f02be1fd7fd9
Am0gUsSV2 <roberto.martinalonso@estudiante.uam.es>
2024-09-30 15:40:37 +0200

    Commit inicial

---
+++ P0/regular_expressionsAlvaro.py
@@ -0,0 +1,23 @@
+"""
+Esta es la expresion regular para el ejercicio 0, que se facilita
+a modo de ejemplo:
+"""
+RE0 = "[ab]*a"
+
+"""
+Completa a continuacion las expresiones regulares para los
+ejercicios 1-6:
+"""
+
+RE1 = "[abc]*a[abc]*b[abc]*|[abc]*b[abc]*a[abc]*"
+RE2 = ""
+RE3 =  r"^(www\.uam\.es|moodle\.uam\.es)(/[a-z]+)*/?$"
+RE4 = ""
+RE5 = r"^\d*[1-9]\d*(?:[\+\-\*/](?:\d*[1-9]\d*|\(\d*[1-9]\d*(?:[\+\-\*/]\d*[1-9]\d*)*\)))*$"
+RE6 = ""
+
+"""
+Recuerda que puedes usar el fichero test_p0.py para probar tus
+expresiones regulares.
+"""
+
