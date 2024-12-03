41a9db08831f313a9e6a4d39b89cb5c4b7fa0f07
Am0gUsSV2 <roberto.martinalonso@estudiante.uam.es>
2024-12-03 17:32:26 +0100

    nueva P1
    pasa test_evaluator

---
+++ P1N/src/automaton.py
@@ -0,0 +1,223 @@
+"""
+    Equipo docente de Autómatas y Lenguajes Curso 2024-25
+    Última modificación: 26 de septiembre de 2024
+
+    Implementación de un autómata. Para ello, se van a definir las siguientes clases:
+        - State: Clase que define un estado del autómata.
+        - Transitions: Clase que define el conjunto de transiciones del autómata.
+        - FiniteAutomaton: Clase que define el autómata finito.
+        - REParser: Clase que parsea de expresión regular a autómata.
+"""
+
+from src.state import State
+from src.transitions import Transitions
+from src.utils import is_deterministic
+from collections import deque
+
+class FiniteAutomaton():
+    """
+        Define un autómata finito.
+    """
+
+    def __init__(self, initial_state: State, states: set, symbols: set, transitions: Transitions):
+
+        if initial_state not in states:
+            raise ValueError(
+                f"Initial state {initial_state.name} "
+                f"is not in the set of states",
+            )
+
+        self.initial_state = initial_state
+        self.states = states
+        self.symbols = symbols
+        self.transitions = transitions
+        
+
+    def __eq__(self, other):
+        if not isinstance(other, type(self)):
+            return NotImplemented
+
+        return (
+            self.initial_state == other.initial_state
+            and self.states == other.states
+            and self.symbols == other.symbols
+            and self.transitions == other.transitions
+        )
+
+    def __repr__(self):
+        return (
+            f"{type(self).__name__}("
+            f"initial_state={self.initial_state!r}, "
+            f"states={self.states!r}, "
+            f"symbols={self.symbols!r}, "
+            f"transitions={self.transitions!r})"
+        )
+
+    # BEGIN Funciones relacionadas con las transiciones
+
+    def add_transition(self, start_state: State, symbol: str, end_state: State):
+        self.transitions.add_transition(start_state, symbol, end_state)
+        
+    def add_transitions(self, transitions: list):
+        self.transitions.add_transitions(transitions)
+
+    def has_transition(self, state: State, symbol: str):
+        return self.transitions.state_has_any_transition_with_symbol(state, symbol)
+        
+    def get_transition(self, state: State, symbol: str):
+        return self.transitions.goes_to(state, symbol)
+        
+    def get_all_transitions(self):
+        return self.transitions.get_all_transitions()
+
+    # END Funciones relacionadas con las transiciones
+
+
+    # BEGIN Funciones relacionadas con el procesamiento de una cadena
+
+    def reset(self):
+        """
+            Esta función resetea el autómata, volviendo al estado inicial
+
+            Args: No args
+
+            Returns: No return
+        """
+        current_states = {self.initial_state}
+        self.current_states = self._complete_lambdas(current_states)
+
+    def process_symbol(self, symbol: str):
+        """
+            Esta función procesa un símbolo
+
+            Args:
+                symbol: símbolo a consumir
+
+            Returns: No return
+        """
+        #------------------------------------------------
+        # TO-DO por el estudiante
+        #-------------------------------------------------
+        new_states = set()
+            
+        # Iterar sobre los estados actuales
+        for state in self.current_states:
+            new_s = self.transitions.goes_to(state, symbol)
+            if new_s:
+                new_states.update(new_s)
+            
+        # Si se encontraron nuevos estados, actualizar los estados actuales
+        if new_states:
+            self.current_states = self._complete_lambdas(new_states)
+        else:
+            self.current_states = set()
+
+
+    def accepts(self, string: str):
+        """
+            Esta función procesa una cadena y comprueba si debe ser aceptada
+              o no.
+
+            Args:
+                string: cadena a procesar
+
+            Returns:
+                True si la cadena debe ser aceptada
+                False otherwise
+        """
+        #--------------------------------------------------
+        # TO-DO por el estudiante
+        #--------------------------------------------------
+        self.reset()
+        for symbol in string:
+            self.process_symbol(symbol)
+            
+            if not self.current_states:
+                print("Cadena no válida: " + string)
+                return False
+        
+        for state in self.current_states:
+            if state.is_final:
+                return True
+        return False
+
+    def _complete_lambdas(self, raw_current_states: set):
+        """
+            Esta función añade al conjunto de estados actuales todos aquellos
+              estados que pueden ser alcanzados con el símbolo lambda.
+
+            Args:
+                raw_current_states: conjunto de estados actuales al que hay
+                  que añadir todos aquellos estados a los que se puede transitar
+                  con el símbolo lambda
+
+            Return:
+                Conjunto de estados actualizado
+
+            Observación:
+                - Recomendable utilizar el método get_lambda_transitions de Transitions
+                
+        """
+        #----------------------------------------------
+        # TODO por el estudiante
+        #----------------------------------------------
+        transciones = self.transitions.get_lambda_transitions()
+        
+        new_states = set(raw_current_states)
+        
+        queue = deque(raw_current_states)
+        
+        while queue:
+            state = queue.popleft()
+            
+            for next_state in transciones.get(state, set()):
+                if next_state not in new_states:
+                    new_states.add(next_state)
+                    queue.append(next_state)
+        
+        return new_states
+    
+    # END Funciones relacionadas con el procesamiento de una cadena
+
+
+    # BEGIN Funciones relacionadas con generar el AFD
+
+    def to_deterministic(self):
+        """
+            Esta función construye un Autómata Finito Determinista a partir del
+              automata original.
+
+            Args: No args
+
+            Return:
+                Un autómata finito determinista
+        """
+        #--------------------------------------------------
+        # TO-DO por el estudiante
+
+        #--------------------------------------------------
+        return FiniteAutomaton(dfa_initial_state, dfa_states, dfa_symbols, dfa_transitions)
+
+    # END Funciones relacionadas con generar el AFD
+
+    # BEGIN Funciones relacionadas con minimizar el AFD
+
+    def to_minimized(self):
+        """
+            Esta función minimiza un Autómata Finito Determinista"
+
+            Args: No args
+
+            Return:
+                Un autómata finito determinista mínimo
+        """
+        if not is_deterministic(self):
+            raise ValueError("The automaton must be deterministic")
+
+        #-------------------------------------------------
+        # TO-DO por el estudiante
+
+        #-------------------------------------------------
+        return FiniteAutomaton(dfam_initial_state, dfam_states, dfam_symbols, dfam_transitions)
+    
+    # END Funciones relacionadas con minimizar el AFD
---
+++ P1N/src/transitions.py
@@ -0,0 +1,265 @@
+"""
+    Equipo docente de Autómatas y Lenguajes Curso 2024-25
+    Última modificación: 26 de septiembre de 2024
+
+    Implementación de un autómata. Para ello, se van a definir las siguientes clases:
+        - State: Clase que define un estado del autómata.
+        - Transitions: Clase que define el conjunto de transiciones del autómata.
+        - FiniteAutomaton: Clase que define el autómata finito.
+        - REParser: Clase que parsea de expresión regular a autómata.
+"""
+
+from src.state import State
+
+class Transitions():
+    """
+    Define el conjunto de transiciones de un autómata. Contiene exclusivamente un
+      diccionario de transiciones y el conjunto de métodos para trabajar con ellas.
+
+    Ejemplo:
+        {
+            q1: {'a': {q1, q2}, 'lambda':{q3}}
+            q2: {'a': {q3}, }
+            q3: {'lambda':{q1}, 'b':{q2}}
+        }
+
+    Observación: Un estado puede ir a varios estados con un mismo símbolo. En el
+      ejemplo anterior, de q1 con el símbolo 'a' se puede ir a q1 y a q2.
+    """
+
+    def __init__(self):
+        self.transitions = {}
+
+
+    def __eq__(self, other):
+        if not isinstance(other, type(self)):
+            return NotImplemented
+
+        return self.transitions == other.transitions
+
+
+    def __repr__(self):
+        return (
+            f"{type(self).__name__}("
+            f"{[f'{key}:{self.transitions[key]}' for key in self.transitions.keys()]})"
+        )
+
+
+    def state_has_transitions(self, state: State):
+        """
+            Esta función comprueba si hay alguna transición desde el estado state.
+                state -- [?] --> ?
+
+            Args:
+                state: estado inicial de la transición
+
+            Returns:
+                True si hay alguna transición
+                False otherwise
+        """
+        #--------------------------------------------------
+        # TODO por el estudiante
+
+        #--------------------------------------------------
+        ret = self.transitions.get(state)
+        if ret is not False:
+            return True
+        return False
+
+    def state_get_symbols(self, state: State):
+        """
+            Esta función devuelve los símbolos con los que se puede transitar desde
+              el estado state.
+
+            Args:
+                state: estado de la transición
+
+            Returns:
+                Lista con los símbolos con los que se puede transitar
+                None en caso de no haber ningún símbolo
+
+            Observación:
+                Recomendable usar la función state_has_transitions
+        """
+        #--------------------------------------------------
+        # TODO por el estudiante
+        #--------------------------------------------------
+        if self.state_has_transitions(state):
+            dict_state = self.transitions.get(state) #Esto deberia devolver el valor de la clave state (que es otro diccionario con claves simbolos y valor estados a donde se puede transicionar)
+            if dict_state is None:
+                return None
+            return list(dict_state.keys()) #Esto deberia devolver la lista con claves (que son los simbolos con los cuales se puede transicionar a otros estados)
+        return None
+
+
+    def state_has_any_transition_with_symbol(self, state: State, symbol: str):
+        """
+            Esta función comprueba si hay alguna transición desde el estado state
+              con el símbolo symbol
+                existe state -- [symbol] --> ?
+
+            Args:
+                state: estado inicial de la transición
+                symbol: símbolo de la transición
+
+            Returns:
+                True si hay alguna transición con el símbolo
+                False otherwise
+
+            Observación:
+                - Recomendable usar la función state_has_transitions
+        """
+        #------------------------------------------------------
+        # TODO por el estudiante
+        #------------------------------------------------------
+        symbols = self.state_get_symbols(state) #Esta funcion ya llama a state_has_transitions
+        if symbols is None:
+            return False
+        if symbol in symbols:
+            return True
+        return False
+
+
+    def state_has_transition_to(self, start_state: State, symbol: str, end_state: State):
+        """
+            Esta función comprueba si hay alguna transición desde el estado start_state
+              con el símbolo symbol al estado end_state
+                existe start_state -- [symbol] --> end_state
+
+            Args:
+                start_state: estado inicial de la transición
+                symbol: símbolo de la transición
+                end_state: estado final de la transición
+
+            Returns:
+                True si existe la transición
+                False otherwise
+
+            Observación:
+                - Recomendable usar la función state_has_any_transition_with_symbol
+        """
+        #-------------------------------------------------------
+        # TODO por el estudiante
+        #-------------------------------------------------------
+        if self.state_has_any_transition_with_symbol(start_state, symbol):
+            states = self.goes_to(start_state, symbol)
+            if end_state in states:
+                return True
+                
+        return False
+
+    def goes_to(self, state: State, symbol: str):
+        """
+            Esta función devuelve los estados a los que se transita desde el estado
+              state con el símbolo symbol
+
+            Args:
+                state: estado inicial de la transición
+                symbol: símbolo de la transición
+
+            Returns:
+                Set de estados a los que se transita. Ejemplo: {q1, q2}
+                None en caso de no existir
+
+            Observación:
+                - Recomendable usar la función state_has_any_transition_with_symbol
+        """
+        #-------------------------------------------------------
+        # TODO por el estudiante
+        #-------------------------------------------------------
+        if self.state_has_any_transition_with_symbol(state, symbol):
+            return self.transitions.get(state).get(symbol)
+        return None
+
+        
+    def add_transition(self, start_state: State, symbol: str, end_state: State):
+        """
+            Esta función añade una transición desde el estado start_state con el símbolo
+              symbol al estado end_state en caso de no existir previamente.
+                start_state -- [symbol] --> end_state
+
+            Args:
+                start_state: estado inicial de la transición
+                symbol: símbolo de la transición
+                end_state: estado final de la transición
+            Returns: No return
+            Raise Exception:
+                - ValueError si la transición ya existe
+
+            Observación:
+                - Recomendable usar las funciones:
+                    state_has_transition_to
+                    state_has_any_transition_with_symbol
+                    state_has_transitions
+        """
+
+        if self.state_has_transition_to(start_state, symbol, end_state):
+            raise ValueError("Repeated transition for state '%s'"%start_state)
+        
+        #----------------------------------------------------------
+        # TODO por el estudiante
+        #----------------------------------------------------------
+        if start_state not in self.transitions:
+            self.transitions[start_state] = {}
+        
+        if self.state_has_any_transition_with_symbol(start_state, symbol) == False:
+            self.transitions[start_state][symbol] = set()
+        
+        self.transitions[start_state][symbol].add(end_state)
+         
+            
+    def add_transitions(self, transitions: list):
+        """
+            Esta función añade un conjunto de transiciones a la vez
+
+            Args:
+                transitions: lista de transiciones en formato tupla (start, symbol, end)
+            Returns: No return
+        """
+        for (start_state, symbol, end_state) in transitions:
+            self.add_transition(start_state, symbol, end_state)
+        
+    
+    def get_all_transitions(self):
+        """
+            Esta función devuelve todas las transiciones en formato lista de tuplas 
+              (start, symbol, end)
+
+            Returns:
+                Lista de tuplas (start, symbol, end)
+        """
+        all_transitions = []
+
+        for start_state in self.transitions:
+            for symbol in self.transitions[start_state]:
+                for end_state in self.transitions[start_state][symbol]:
+                    all_transitions.append((start_state, symbol, end_state))
+        
+        return all_transitions
+
+
+    def get_lambda_transitions(self):
+        """
+            Esta función devuelve un diccionario con todas las transiciones lambda
+              start_state -- [lambda] --> end_states
+            en un formato simplificado
+              start_state --> end_states
+
+            Args: No args
+
+            Return:
+                Diccionario de transiciones con lambda.
+                  Clave: estado de inicio de transición
+                  Valor: set de estados destino
+
+            Observación:
+                - El símbolo lambda se representa con None
+        """
+        lambda_transitions = {}
+
+        for start_state in self.transitions:
+            if self.state_has_any_transition_with_symbol(start_state, None):
+                end_states = self.goes_to(start_state, None)
+                lambda_transitions[start_state] = end_states
+
+        return lambda_transitions
