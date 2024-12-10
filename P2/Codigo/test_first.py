6b95dc91156d7ea2e3c9f6c63aa41a14e715327c
Alvaro578578 <126505188+Alvaro578578@users.noreply.github.com>
2024-12-10 20:33:05 +0100

    Update test_first.py --> mas tests

--- P2/Codigo/test_first.py
+++ P2/Codigo/test_first.py
@@ -39,6 +39,65 @@ class TestFirst(unittest.TestCase):
         self._check_first(grammar, "Y+i", {'+', '*'})
         self._check_first(grammar, "YX", {'+', '*', ''})
         self._check_first(grammar, "YXT", {'+', '*', 'i', '('})
+    
+    def test_case2(self) -> None:
+        """Test Case 2."""
+        grammar_str = """
+        S -> aBC
+        B -> b
+        B -> 
+        C -> cD
+        C -> 
+        D -> d
+        D -> 
+        """
+
+        grammar = GrammarFormat.read(grammar_str)
+        self._check_first(grammar, "S", {'a'})
+        self._check_first(grammar, "B", {'b', ''})
+        self._check_first(grammar, "C", {'c', ''})
+        self._check_first(grammar, "D", {'d', ''})
+        self._check_first(grammar, "BC", {'b', 'c', ''})
+        self._check_first(grammar, "aBC", {'a'})
+        self._check_first(grammar, "BCD", {'b', 'c', 'd', ''})
+    
+    def test_case3(self) -> None:
+        """Test Case 3."""
+        grammar_str = """
+        S -> AB
+        A -> aA
+        A -> 
+        B -> b
+        B -> c
+        """
+
+        grammar = GrammarFormat.read(grammar_str)
+        self._check_first(grammar, "A", {'a', ''})
+        self._check_first(grammar, "B", {'b', 'c'})
+        self._check_first(grammar, "S", {'a', 'b', 'c'})
+        self._check_first(grammar, "AB", {'a', 'b', 'c'})
+        self._check_first(grammar, "aB", {'a'})
+        self._check_first(grammar, "Ab", {'a', 'b'})
+    
+    def test_case4(self) -> None:
+        grammar_str = """
+        S -> aS
+        S -> bA
+        A -> cA
+        A -> d
+        """
+
+        grammar = GrammarFormat.read(grammar_str)
+        self._check_first(grammar, "S", {'a', 'b'})
+        self._check_first(grammar, "A", {'c', 'd'})
+        self._check_first(grammar, "aS", {'a'})
+        self._check_first(grammar, "bA", {'b'})
+        self._check_first(grammar, "cA", {'c'})
+        self._check_first(grammar, "d", {'d'})
+
+
+
+
 
 
 if __name__ == '__main__':
